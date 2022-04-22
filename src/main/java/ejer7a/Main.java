c467174ce4185ec8e1a86681a3baa1d147e5747b
alvaro <alvaromarflo@gmail.com>
2022-04-22 14:38:24 +0200

    ppp

--- src/main/java/ejer7a/Main.java
+++ src/main/java/ejer7a/Main.java
@@ -1,14 +1,10 @@
 package ejer7a;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
