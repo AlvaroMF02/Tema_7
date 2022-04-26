23ee648363dfc5d24372ceee4f1fc76673d8457b
alvaro <alvaromarflo@gmail.com>
2022-04-26 22:54:18 +0200

    aaaa

--- src/main/java/ejer4_coche/ejer4.java
+++ src/main/java/ejer4_coche/ejer4.java
@@ -7,77 +7,64 @@ import java.util.ArrayList;
 
 /**
  *
- * @author alvaro
+ * @author copiado entendiendo :)
  */
 public class ejer4 {
+
     public static void main(String[] args) {
+        // Fichero a crear. Ruta relativa a la carpeta raíz del proyecto
+        String idFichero = "coches.txt";
+        ArrayList<Vehiculo> listaCoches = new ArrayList();
         
-       String idFichero="vehiculos.txt";
-       ArrayList<Vehiculo> coches = new ArrayList<>();
+            //Turismos
+            listaCoches.add(new Turismo(Long.MAX_VALUE, "61512824", "fiat", "A4", "amarillo", 1651.36, 2, true));
+            listaCoches.add(new Turismo(Long.MAX_VALUE, "65846546", "audi", "9M", "rojo", 1651.32, 4, true));
+            listaCoches.add(new Turismo(Long.MAX_VALUE, "89658528", "fiat", "P0", "verde", 6485.25, 2, true));
+            listaCoches.add(new Turismo(Long.MAX_VALUE, "12542862", "audi", "9M", "azul", 1651.36, 4, true));
+            listaCoches.add(new Turismo(Long.MAX_VALUE, "36988585", "fiat", "O9", "negro", 1651.36, 4, true));
+            listaCoches.add(new Turismo(Long.MAX_VALUE, "25878528", "audi", "A4", "blanco", 1651.36, 2, true));
+            listaCoches.add(new Turismo(Long.MAX_VALUE, "14782855", "fiat", "9M", "naranja", 1651.36, 4, true));
+            listaCoches.add(new Turismo(Long.MAX_VALUE, "35135135", "audi", "V6", "morado", 61516.36, 2, false));
+            listaCoches.add(new Turismo(Long.MAX_VALUE, "61514252", "fiat", "A4", "rosa", 88555.25, 4, true));
+            listaCoches.add(new Turismo(Long.MAX_VALUE, "61514524", "audi", "C3", "azul", 999.99, 4, true));
 
-       
-       try (BufferedWriter flujo = new BufferedWriter(new FileWriter(idFichero))){
-            
-        //Turismos
-        coches.add(new Turismo(Long.MAX_VALUE, "61512824", "fiat", "A4", "amarillo", 1651.36, 2, true));
-        coches.add(new Turismo(Long.MAX_VALUE, "65846546", "audi", "9M", "rojo", 1651.32, 4, true));
-        coches.add(new Turismo(Long.MAX_VALUE, "89658528", "fiat", "P0", "verde", 6485.25, 2, true));
-        coches.add(new Turismo(Long.MAX_VALUE, "12542862", "audi", "9M", "azul", 1651.36, 4, true));
-        coches.add(new Turismo(Long.MAX_VALUE, "36988585", "fiat", "O9", "negro", 1651.36, 4, true));
-        coches.add(new Turismo(Long.MAX_VALUE, "25878528", "audi", "A4", "blanco", 1651.36, 2, true));
-        coches.add(new Turismo(Long.MAX_VALUE, "14782855", "fiat", "9M", "naranja", 1651.36, 4, true));
-        coches.add(new Turismo(Long.MAX_VALUE, "35135135", "audi", "V6", "morado", 61516.36, 2, false));
-        coches.add(new Turismo(Long.MAX_VALUE, "61514252", "fiat", "A4", "rosa", 88555.25, 4, true));
-        coches.add(new Turismo(Long.MAX_VALUE, "61514524", "audi", "C3", "azul", 999.99, 4, true));
-
-        //Furgonetas
-        coches.add(new Furgoneta(Long.MAX_VALUE, "55162752", "seat", "OP4", "amarillo", 88555.25, 654, 235));
-        coches.add(new Furgoneta(Long.MAX_VALUE, "55527516", "ducato", "LÑS", "rojo", 61516.36, 654, 235));
-        coches.add(new Furgoneta(Long.MAX_VALUE, "55725516", "seat", "87Y", "azul", 1651.36, 654, 235));
-        coches.add(new Furgoneta(Long.MAX_VALUE, "25425516", "ducato", "ASD8", "amarillo", 88555.25, 654, 235));
-        coches.add(new Furgoneta(Long.MAX_VALUE, "42525516", "seat", "SADA9", "blanco", 61516.36, 654, 235));
-        coches.add(new Furgoneta(Long.MAX_VALUE, "55164524", "ducato", "RG3", "azul", 1651.36, 654, 235));
-        coches.add(new Furgoneta(Long.MAX_VALUE, "55452416", "seat", "LO3", "morado", 61516.36, 654, 235));
-        coches.add(new Furgoneta(Long.MAX_VALUE, "55452416", "ducato", "ÑP9", "amarillo", 88555.25, 654, 235));
-        coches.add(new Furgoneta(Long.MAX_VALUE, "55242116", "seat", "PN5", "naranja", 1651.36, 654, 235));
-        coches.add(new Furgoneta(Long.MAX_VALUE, "57417516", "ducato", "OGT", "amarillo", 61516.36, 654, 235));
+            //Furgonetas
+            listaCoches.add(new Furgoneta(Long.MAX_VALUE, "55162752", "seat", "OP4", "amarillo", 88555.25, 654, 235));
+            listaCoches.add(new Furgoneta(Long.MAX_VALUE, "55527516", "ducato", "LÑS", "rojo", 61516.36, 654, 235));
+            listaCoches.add(new Furgoneta(Long.MAX_VALUE, "55725516", "seat", "87Y", "azul", 1651.36, 654, 235));
+            listaCoches.add(new Furgoneta(Long.MAX_VALUE, "25425516", "ducato", "ASD8", "amarillo", 88555.25, 654, 235));
+            listaCoches.add(new Furgoneta(Long.MAX_VALUE, "42525516", "seat", "SADA9", "blanco", 61516.36, 654, 235));
+            listaCoches.add(new Furgoneta(Long.MAX_VALUE, "55164524", "ducato", "RG3", "azul", 1651.36, 654, 235));
+            listaCoches.add(new Furgoneta(Long.MAX_VALUE, "55452416", "seat", "LO3", "morado", 61516.36, 654, 235));
+            listaCoches.add(new Furgoneta(Long.MAX_VALUE, "55452416", "ducato", "ÑP9", "amarillo", 88555.25, 654, 235));
+            listaCoches.add(new Furgoneta(Long.MAX_VALUE, "55242116", "seat", "PN5", "naranja", 1651.36, 654, 235));
+            listaCoches.add(new Furgoneta(Long.MAX_VALUE, "57417516", "ducato", "OGT", "amarillo", 61516.36, 654, 235));
 
 
-        //Deportivos
-        coches.add(new Deportivo(Long.MAX_VALUE, "61582754", "lambogini", "OGT", "rojo", 31651, 965));
-        coches.add(new Deportivo(Long.MAX_VALUE, "62452158", "siu", "PN5", "rojo", 31651, 965));
-        coches.add(new Deportivo(Long.MAX_VALUE, "61538758", "lambogini", "OGT", "rojo", 31651, 965));
-        coches.add(new Deportivo(Long.MAX_VALUE, "64524158", "siu", "PN5", "rojo", 31651, 965));
-        coches.add(new Deportivo(Long.MAX_VALUE, "61724458", "lambogini", "OGT", "rojo", 31651, 965));
-        coches.add(new Deportivo(Long.MAX_VALUE, "62452158", "siu", "PN5", "rojo", 31651, 965));
-        coches.add(new Deportivo(Long.MAX_VALUE, "64524158", "lambogini", "PN5", "rojo", 31651, 965));
-        coches.add(new Deportivo(Long.MAX_VALUE, "45246158", "lambogini", "PN5", "rojo", 31651, 965));
-        coches.add(new Deportivo(Long.MAX_VALUE, "61582454", "siu", "OGT", "rojo", 31651, 965));
-        coches.add(new Deportivo(Long.MAX_VALUE, "61524458", "lambogini", "ñ", "rojo", 31651, 965));
+            //Deportivos
+            listaCoches.add(new Deportivo(Long.MAX_VALUE, "61582754", "lambogini", "OGT", "rojo", 31651, 965));
+            listaCoches.add(new Deportivo(Long.MAX_VALUE, "62452158", "siu", "PN5", "rojo", 31651, 965));
+            listaCoches.add(new Deportivo(Long.MAX_VALUE, "61538758", "lambogini", "OGT", "rojo", 31651, 965));
+            listaCoches.add(new Deportivo(Long.MAX_VALUE, "64524158", "siu", "PN5", "rojo", 31651, 965));
+            listaCoches.add(new Deportivo(Long.MAX_VALUE, "61724458", "lambogini", "OGT", "rojo", 31651, 965));
+            listaCoches.add(new Deportivo(Long.MAX_VALUE, "62452158", "siu", "PN5", "rojo", 31651, 965));
+            listaCoches.add(new Deportivo(Long.MAX_VALUE, "64524158", "lambogini", "PN5", "rojo", 31651, 965));
+            listaCoches.add(new Deportivo(Long.MAX_VALUE, "45246158", "lambogini", "PN5", "rojo", 31651, 965));
+            listaCoches.add(new Deportivo(Long.MAX_VALUE, "61582454", "siu", "OGT", "rojo", 31651, 965));
+            listaCoches.add(new Deportivo(Long.MAX_VALUE, "61524458", "lambogini", "ñ", "rojo", 31651, 965));
 
+        try ( BufferedWriter flujo = new BufferedWriter(new FileWriter(idFichero))) {
 
-
-            for (int i = 0; i < coches.size(); i++) {
-                if (coches.get(i) instanceof Turismo) {
-                    flujo.write("0- "+coches.get(i).toString()+":");
-                    flujo.newLine();
-                }else if (coches.get(i) instanceof Furgoneta){
-                    flujo.write("1- "+coches.get(i).toString()+":");
-                    flujo.newLine();
-                }else{
-                    flujo.write("2- "+coches.get(i).toString()+":");
-                    flujo.newLine();
-                }
-                    
+            for (int i = 0; i < listaCoches.size(); i++) {
+                flujo.write(listaCoches.get(i).toString() + ";");
+                flujo.newLine();
             }
-
-
+            // Metodo fluh() guarda cambios en disco 
             flujo.flush();
+            System.out.println("Fichero " + idFichero + " creado correctamente.");
         } catch (IOException e) {
             System.out.println(e.getMessage());
-       }
-       
-       
+        }
     }
-    
+
 }
--- src/main/java/ejer4_coche/ejer7.java
+++ src/main/java/ejer4_coche/ejer7.java
@@ -1,26 +1,92 @@
 package ejer4_coche;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
  *
- * @author alvaro
+ * @author copiado entendiendo :)
  */
 
-/*
-Realiza un programa que lea los datos del ejercicio 4. Para ello creará una 
-lista de objetos de tipo Vehículo. El programa irá almacenando en la lista 
-los objetos leídos desde el archivo de texto “vehículos.txt”. Una vez cargados 
-todos los datos en la lista, ordena los vehículos por Marca y muestra el 
-resultado por consola.
-*/
 public class ejer7 {
-     public static void main(String[] args) {
+
+    /**
+     * @param args the command line arguments
+     */
+    //Método el cual escribe en un csv de turismos
+    private static void escribirFicheroTurismo(ArrayList<Turismo> listaTurismo) {
+        //Escribimos el fichero de turismos
+        String idFichero = "Turismos.csv";
+        try (BufferedWriter flujo = new BufferedWriter(new FileWriter(idFichero))) {
+            flujo.write("Bastidor;Matricula;Marca;Modelo;Color;Tarifa;Disponible;Puertas;Marcha Automatica");
+            flujo.newLine();
+            for (Turismo turismo : listaTurismo) {
+                flujo.write(turismo.getAtributos());
+                flujo.newLine();
+            }
+            // Metodo fluh() guarda cambios en disco 
+            flujo.flush();
+            System.out.println("Fichero " + idFichero + " creado correctamente.");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    //Método el cual escribe en un csv de deportivos
+    private static void escribirFicheroDeportivo(ArrayList<Deportivo> listaDeportivo) {
+        //Escribimos el fichero de turismos
+        String idFichero = "Deportivos.csv";
+        try (BufferedWriter flujo = new BufferedWriter(new FileWriter(idFichero))) {
+            flujo.write("Bastidor;Matricula;Marca;Modelo;Color;Tarifa;Disponible;Cilindrada");
+            flujo.newLine();
+            for (Deportivo deportivo : listaDeportivo) {
+                flujo.write(deportivo.getAtributos());
+                flujo.newLine();
+            }
+            // Metodo fluh() guarda cambios en disco 
+            flujo.flush();
+            System.out.println("Fichero " + idFichero + " creado correctamente.");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    //Método el cual escribe en un csv de furgonetas
+    private static void escribirFicheroFurgoneta(ArrayList<Furgoneta> listaFurgoneta) {
+        //Escribimos el fichero de turismos
+        String idFichero = "Furgonetas.csv";
+        try (BufferedWriter flujo = new BufferedWriter(new FileWriter(idFichero))) {
+            flujo.write("Bastidor;Matricula;Marca;Modelo;Color;Tarifa;Disponible;Carga;Volumen");
+            flujo.newLine();
+            for (Furgoneta furgoneta : listaFurgoneta) {
+                flujo.write(furgoneta.getAtributos());
+                flujo.newLine();
+            }
+            // Metodo fluh() guarda cambios en disco 
+            flujo.flush();
+            System.out.println("Fichero " + idFichero + " creado correctamente.");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void escribirFicheros(ArrayList<Turismo> listaTurismo, ArrayList<Deportivo> listaDeportivo, ArrayList<Furgoneta> listaFurgoneta) {
+        escribirFicheroTurismo(listaTurismo);
+        escribirFicheroDeportivo(listaDeportivo);
+        escribirFicheroFurgoneta(listaFurgoneta);
+
+    }
+
+    public static void main(String[] args) {
+
+        //LECTURA EJERCICIO 7
         // Fichero a leer con datos de ejemplo
-        String idFichero = "vehiculos.txt";
+        String idFichero = "coches.txt";
 
         // Variables para guardar los datos que se van leyendo
         String[] tokens;
@@ -35,7 +101,9 @@ public class ejer7 {
 
         System.out.println("Leyendo el fichero: " + idFichero);
 
-
+        // Inicialización del flujo "datosFichero" en función del archivo llamado "idFichero"
+        // Estructura try-with-resources. Permite cerrar los recursos una vez finalizadas
+        // las operaciones con el archivo
         try (Scanner datosFichero = new Scanner(new File(idFichero))) {
 
             datosFichero.nextLine();
@@ -71,8 +139,9 @@ public class ejer7 {
                         d1.setModelo(tokens[4]);
                         d1.setColor(tokens[5]);
                         d1.setTarifa(Double.parseDouble(tokens[6]));
-                        d1.setCilindrada(Integer.parseInt(tokens[8]));
-                        
+                        d1.setDisponible(Boolean.valueOf(tokens[7]));
+                        d1.setCilindrada(Integer.parseInt(tokens[8].substring(0, tokens[8].length() - 1)));
+
                         listaVehiculo.add(d1);
                         listaDeportivo.add(d1);
                         break;
@@ -84,8 +153,9 @@ public class ejer7 {
                         f1.setModelo(tokens[4]);
                         f1.setColor(tokens[5]);
                         f1.setTarifa(Double.parseDouble(tokens[6]));
-                        f1.setCarga(Integer.parseInt(tokens[7]));
-                        f1.setVolumen(Integer.parseInt(tokens[8]));
+                        f1.setDisponible(Boolean.valueOf(tokens[7]));
+                        f1.setCarga(Integer.parseInt(tokens[8]));
+                        f1.setVolumen(Integer.parseInt(tokens[9].substring(0, tokens[9].length() - 1)));
                         listaVehiculo.add(f1);
                         listaFurgoneta.add(f1);
                         break;
@@ -97,13 +167,14 @@ public class ejer7 {
             System.out.println(e.getMessage());
         }
 
-        //Collections.sort(listaVehiculo);
-
+//        Collections.sort(listaVehiculo);
         for (Vehiculo lista : listaVehiculo) {
             System.out.println(lista);
 
         }
+
+        //ESCRITURA EJERCICIO 9
+        escribirFicheros(listaTurismo, listaDeportivo, listaFurgoneta);
     }
-    
-    
+
 }
